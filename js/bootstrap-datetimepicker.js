9c017c918fce04ece29498c0b63d99bb3f57f54f
David Patterson <dpatterson2008@hotmail.com>
2018-12-12 20:56:56 +0000

    Ignore calculating containerOffsetTop

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -638,7 +638,7 @@
         top = offset.top + this.height;
       }
 
-      top = top - containerOffset.top;
+      top = top //- containerOffset.top;
       left = left - containerOffset.left;
 
       this.picker.css({
